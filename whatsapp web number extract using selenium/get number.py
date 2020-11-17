e43545d64b506dfefa2868bdb93f244f99c12018
Sandeep Gumaste <73634237+SandeepGumaste@users.noreply.github.com>
2020-11-18 00:11:22 +0530

    Updated

--- whatsapp web number extract using selenium/get number.py
+++ whatsapp web number extract using selenium/get number.py
@@ -1,27 +1,36 @@
 import pyautogui
+import csv
 from selenium import webdriver
+from datetime import date
+dat = str(date.today())
+fn = dat + '.csv'
 driver = webdriver.Chrome()
 driver.get('https://web.whatsapp.com/')
 driver.maximize_window()
 pyautogui.FAILSAFE = False
-pyautogui.alert("Press Enter when the page loads fully after ")
+pyautogui.alert("Press Enter when the page loads fully after scanning qr code")
 (x,y) = pyautogui.position()
 pyautogui.move(-x,-y)
-pyautogui.move(607,1038)
+pyautogui.move(607,1050)
 lst = list()
-n = 60 #input("number of clicks")
+n = 30 #input("number of clicks")
 m = 0
-l = list()
+cl = list() #Contact list
 while m < n:
     try:
         res = driver.find_elements_by_xpath("//span[starts-with(@title,'+91')]")
         for c in res:
-            l.append(c.text)
-        pyautogui.click(clicks = 3)
-        m = m +1
+            cl.append(c.text)
     except:
-        pyautogui.click(clicks = 3)
-        m = m +1
-l = set(l)
-for cc in l:
-    print(cc)
+        pass
+    pyautogui.click(clicks = 2)
+    m = m +1
+cl = list(set(cl))
+with open(fn ,'w') as csvfile:
+    tw = csv.DictWriter(csvfile ,fieldnames = ['Name','Phone Number'])
+    tw.writeheader()
+    count = 0
+    while count < len(cl):
+        tw.writerow({'Name':cl[count], 'Phone Number':cl[count]})
+        count+=1
+driver.close()
--- whatsapp web number extract using selenium/num_extract.py
+++ whatsapp web number extract using selenium/num_extract.py
@@ -1,14 +1,31 @@
 import pyautogui
+import csv
+from datetime import date
 from selenium import webdriver
+dat = str(date.today())
+fn = dat + '.csv'
 driver = webdriver.Chrome()
 url = 'https://www.webscraper.io/test-sites/e-commerce/allinone'
 driver.get(url)
 driver.maximize_window()
-driver.execute_script('window.scrollBy(0,500)')
+driver.execute_script('window.scrollBy(0,200)')
 pyautogui.FAILSAFE = False
 xp = "//h4[starts-with(@class,'pull')]"
-
-res = driver.find_elements_by_xpath(xp)
-for obj in res :
-    print(obj.text)
+xt = "//a[contains(@class,'title')]"
+pp = driver.find_elements_by_xpath(xp)
+tt = driver.find_elements_by_xpath(xt)
+price = list()
+title = list()
+for x in pp:
+    price.append(x.text)
+for x in tt:
+    title.append(x.text)
+with open(fn ,'w') as csvfile:
+    filn = ['Title','Price']
+    tw = csv.DictWriter(csvfile ,fieldnames = filn)
+    tw.writeheader()
+    count = 0
+    while count < len(price):
+        tw.writerow({'Title':title[count], 'Price':price[count]})
+        count+=1
 driver.close()
